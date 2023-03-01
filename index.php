44fdaa8aa33bc067a25b9077b77397da0f37ad04
sarawut <u.sarawut586@gmail.com>
2023-03-01 07:51:04 +0700

    main

--- index.php
+++ index.php
@@ -13,7 +13,38 @@
 
 
 <body class="bg-body">
-    <div class="container">
+    <header>
+        <div class="px-3 py-2 bg-dark text-white">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="#" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+                        <div style="font-size: 30px;"><i class="fa-solid fa-circle-dollar-to-slot"></i></div>
+                    </a>
+
+                    <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                        <li>
+                            <a href="#" class="nav-link text-secondary">
+                                <div class="nav-item">
+                                    <div class="icon"><i class="fa-solid fa-house"></i></div>
+                                    <div class="text"> Home</div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white">
+                                <div class="nav-item">
+                                    <div class="icon"> <i class="fa-solid fa-display"></i></div>
+                                    <div class="text"> Dashboard</div>
+                                </div>
+                            </a>
+                        </li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container my-4">
         <div class="row">
             <div class="col-xxl-12">
                 <div class="row mb-3">
