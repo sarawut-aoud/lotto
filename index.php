f6119509fadf665173c74adec9e65a95b80c5c3c
sarawut <u.sarawut586@gmail.com>
2023-03-01 07:56:23 +0700

    main

--- index.php
+++ index.php
@@ -38,59 +38,68 @@
                                 </div>
                             </a>
                         </li>
-                       
+
                     </ul>
                 </div>
             </div>
         </div>
     </header>
-    <div class="container my-4">
-        <div class="row">
-            <div class="col-xxl-12">
-                <div class="row mb-3">
-                    <div class="col-6">
-                        <div class="content">
-                            <div class="content-pirce">
-                                <div class="text">เบอร์ละ</div>
-                                <div class="price">50 .-</div>
+    <div class="container-fluid my-4">
+        <div class="row ">
+            <div class="row mb-3  justify-content-center">
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="content">
+                                <div class="content-pirce">
+                                    <div class="text">เบอร์ละ</div>
+                                    <div class="price">50 .-</div>
+                                </div>
+                                <div class="content-date">
+                                    <div class="text">1/3/2566</div>
+                                </div>
                             </div>
-                            <div class="content-date">
-                                <div class="text">1/3/2566</div>
+
+                        </div>
+                        <div class="col-6">
+                            <div class="content-pay">
+                                <div class="text">
+                                    <h1>เบอร์เงินสด</h1>
+                                </div>
+                                <div class="top-number">
+                                    ถูกบน 300 .-
+                                </div>
+                                <div class="bottom-number">
+                                    ถูกล่าง 3000 .-
+                                </div>
                             </div>
                         </div>
+                    </div>
 
-                    </div>
-                    <div class="col-6">
-                        <div class="content-pay">
-                            <div class="text">
-                                <h1>เบอร์เงินสด</h1>
-                            </div>
-                            <div class="top-number">
-                                ถูกบน 300 .-
-                            </div>
-                            <div class="bottom-number">
-                                ถูกล่าง 3000 .-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="box-container">
-                        <?php for ($i = 0; $i <= 99; $i++) : ?>
-                            <div class="box-item">
-                                <div class="box-item-tool"></div>
-                                <div class="box-item-number"><?= $i ?></div>
-
-                            </div>
-                        <?php endfor; ?>
-                    </div>
                 </div>
 
             </div>
+            <div class="row mb-3 justify-content-center">
+                <div class="col-10">
+                    <div class="d-grid gap-3" style="grid-template-columns: 1fr 2fr;">
+                        <div class="bg-light border rounded-3">
+                            <div class="box-container">
+                                <?php for ($i = 0; $i <= 99; $i++) : ?>
+                                    <div class="box-item">
+                                        <div class="box-item-tool"></div>
+                                        <div class="box-item-number"><?= $i ?></div>
 
+                                    </div>
+                                <?php endfor; ?>
+                            </div>
+                        </div>
+                        <div class="bg-light border rounded-3">
 
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
 
 
